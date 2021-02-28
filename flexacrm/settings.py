e4ce66403f95f6365a79b1606075bfab55fc62aa
yupeekiyay <76213756+yupeekiyay@users.noreply.github.com>
2021-02-28 22:17:35 +0300

    new appraoch static

--- flexacrm/settings.py
+++ flexacrm/settings.py
@@ -1,5 +1,5 @@
-
 from pathlib import Path
+import os
 import environ
 
 env = environ.Env(
@@ -21,7 +21,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'whitenoise.runserver_nostatic',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -121,13 +121,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'public', 'static')
 STATICFILES_DIRS = [
-    BASE_DIR / "static"
+    os.path.join(BASE_DIR, 'static')
 ]
-MEDIA_URL = '/media/'
-MEDIA_ROOT = "media_root"
-STATIC_ROOT = "static_root"
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 
 AUTH_USER_MODEL = 'leads.User'
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
